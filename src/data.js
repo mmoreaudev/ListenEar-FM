f8a06a9ef2c1df1033902a9e65546f9393ce0c28
Matéo Moreau <devpleven@gmail.com>
2023-02-17 13:12:24 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ function chillHop() {
 		{
 			name: "NRJ",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/nrj.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/nrj.png",
 			artist: "NRJ - Hit Music Only !",
 			audio: "https://scdn.nrjaudio.fm/fr/30001/mp3_128.mp3?cdn_path=adswizz_lbs12&access_token=138ae4ad4b904ffc9b3d7c6983310bc8",
 			color: ["#205950", "#2ab3bf"],
@@ -14,7 +14,7 @@ function chillHop() {
 		{
 			name: "Skyrock",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/skyrock.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/skyrock.png",
 			artist: "Skyrock - Premier sur le rap",
 			audio: "http://icecast.skyrock.net/s/natio_mp3_128k",
 			color: ["#EF8EA9", "#ab417f"],
@@ -24,7 +24,7 @@ function chillHop() {
 		{
 			name: "Nostalgie",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/nostalgie.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/nostalgie.png",
 			artist: "Nostagie - Les plus grands tubes !",
 			audio: "https://scdn.nrjaudio.fm/fr/30601/mp3_128.mp3?origine=fluxradios&cdn_path=adswizz_lbs9&access_token=14ea5f3405de40f79f375fcf742b2e16",
 			color: ["#CD607D", "#c94043"],
@@ -34,7 +34,7 @@ function chillHop() {
 		{
 			name: "Mouv'",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/mouv.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/mouv.png",
 			artist: "Mouv' - Mon époque. Ma radio",
 			audio: "   http://direct.mouv.fr/live/mouv-midfi.mp3",
 			color: ["#EF8EA9", "#ab417f"],
@@ -44,7 +44,7 @@ function chillHop() {
 		{
 			name: "Virgin Radio",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/virginradio.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/virginradio.png",
 			artist: "Virgin Radio - Pop, Rock, Électro !",
 			audio: "http://stream.virginradio.fr/virgin.mp3",
 			color: ["#CD607D", "#c94043"],
@@ -54,7 +54,7 @@ function chillHop() {
 		{
 			name: "RTL 2",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/rtl2.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/rtl2.png",
 			artist: "RTL 2 - Le son Pop-Rock ",
 			audio: "   http://icecast.rtl2.fr/rtl2-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg",
 			color: ["#205950", "#2ab3bf"],
@@ -64,7 +64,7 @@ function chillHop() {
 		{
 			name: "NG Radio",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/ngradio.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/ngradio.png",
 			artist: "Nation Glory Radio",
 			audio: "https://radio.nationsglory.fr:8000/ngradio",
 			color: ["#CD607D", "#c94043"],
@@ -74,7 +74,7 @@ function chillHop() {
 		{
 			name: "Fun Radio",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/funradio.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/funradio.png",
 			artist: "Fun Radio - Le son DANCEFLOOR !",
 			audio: "http://streamer-01.rtl.fr/fun-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg",
 			color: ["#CD607D", "#c94043"],
@@ -84,7 +84,7 @@ function chillHop() {
 		{
 			name: "Nostalgie Génération 80",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/nostalgie80.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/nostalgie80.png",
 			artist: "Nostalgie Génération 80",
 			audio: "http://scdn.nrjaudio.fm/adwz1/fr/55524/mp3_128.mp3?origine=fluxradios",
 			color: ["#CD607D", "#c94043"],
@@ -94,7 +94,7 @@ function chillHop() {
 		{
 			name: "Alouette",
 			cover:
-				"https://api.pleven-dev.fr/radio/logo/alouette.png",
+				"https://raw.githubusercontent.com/mmoreaudev/ListenEar-FM/main/radio/logo/alouette.png",
 			artist: "Alouette - La radio qui plume les Parisiennes",
 			audio: "http://alouette.ice.infomaniak.ch/alouette-high.mp3",
 			color: ["#CD607D", "#c94043"],
